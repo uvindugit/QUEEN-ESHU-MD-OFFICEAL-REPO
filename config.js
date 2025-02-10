a9b036b7b192e1679856c2dd938b650cdd7051bc
ESHAN <165408651+ESHAN-QUEEN-ESHU-MD@users.noreply.github.com>
2025-02-10 16:01:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=LQdjQJxD#gYe1Rc06vqd07oUxwDflNYeQ5-ct4B3H8BcjbzyCC9Y",
+SESSION_ID: process.env.SESSION_ID || "ESHU-MD=your_paircode",
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
